cd312e0455f5f3c76e0c14bbb8f04c9e34510e79
ryanlovell <shovell242@gmail.com>
2017-12-28 19:56:20 -0500

    added validation for user input

--- scrabble.py
+++ scrabble.py
@@ -15,7 +15,7 @@ def loadWords():
     Returns: a list of all words within the file
     '''
     file = open(WORD_LIST_FNAME, 'r')
-    text = file.readlines()
+    text = file.read().splitlines()
     return text
 
 def dealHand():
@@ -24,6 +24,31 @@ def dealHand():
     based on the 'WORDS_INV_TOTAL' as quantity of random characters
     '''
     return random.sample(SCRABBLE_LETTER_VALUES.keys(), WORDS_INV_TOTAL)
+
+def validateUserGuess(hand, guess):
+    '''
+    Input: current hand of characters user is choosing from to create words
+    as well as the actual word guess of the user
+    Returns: True or False if the user has used only valid characters in their guess
+    '''
+    handCopy = hand.copy()
+    for char in guess:
+        if char in handCopy:
+            handCopy.remove(char)
+        else:
+            return False
+    return True
+
+def validateWordExists(guess):
+    '''
+    Input: takes the users guess after validation of characaters
+    and see if it matches any words within the our loaded words.txt
+    Return: True or False if the word....is in fact a word
+    '''
+    return guess.upper() in loadWords()
+
+
+
 
 
 
