fdaa0e56f8dadaea2ded3375cff80fd43c75f3fd
ryanlovell <shovell242@gmail.com>
2017-12-28 21:58:48 -0500

    game structure almost finished

--- scrabble.py
+++ scrabble.py
@@ -1,6 +1,5 @@
 import random
 
-
 SCRABBLE_LETTER_VALUES = {
     'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
 }
@@ -31,10 +30,9 @@ def validateUserGuess(hand, guess):
     as well as the actual word guess of the user
     Returns: True or False if the user has used only valid characters in their guess
     '''
-    handCopy = hand.copy()
     for char in guess:
-        if char in handCopy:
-            handCopy.remove(char)
+        if char in hand:
+            hand.remove(char)
         else:
             return False
     return True
@@ -46,6 +44,75 @@ def validateWordExists(guess):
     Return: True or False if the word....is in fact a word
     '''
     return guess.upper() in loadWords()
+
+def scoreUserGuess(guess):
+    '''
+    Input: string of the guess
+    Returns: using the SCRABBLE_LETTER_VALUES constant, this will sum up
+    the users guess with associated point total and add a bonus of 50 if 
+    the WORDS_INV_TOTAL is used
+    '''
+    scoreTotal = 0
+    for char in guess:
+        scoreTotal += SCRABBLE_LETTER_VALUES[char]
+    if len(guess) == WORDS_INV_TOTAL:
+        scoreTotal = (scoreTotal * len(guess)) + 50
+    else:
+        scoreTotal *= len(guess)
+    return scoreTotal
+
+def userMenu():
+    '''
+    prints options for users between guesses during the game
+    Returns: a character of user choice
+    '''
+    viableAnswers = ['n', 'r', 'e']
+    while True:
+        ans = input('Enter n to deal a new hand, r to replay the last hand, or e to end game: ')
+        if ans in viableAnswers:
+            break
+        else:
+            print('Sorry, that is not a viable answer. Please choose again')
+    return ans
+
+def gameInput():
+    '''
+    Input: the user will enter his word guesses here or a '.' to indicate 
+    they are done guessing
+    Returns: the user word guess or '.' to end guessing
+    '''
+    ans = input('Enter word, or a "." to indicate that you are finished: ')
+    return ans
+
+def game(prevHand = False):
+    if prevHand:
+        hand = prevHand
+    else:
+        hand = dealHand()
+    score = 0
+    while True:
+        print('Current Hand:', ' '.join(hand))
+        guess = gameInput()
+        if guess == '.':
+            break
+        if not validateUserGuess(hand, guess) and not validateWordExists(guess):
+            print('Invalid word, please try again.')
+        else:
+            points = scoreUserGuess(guess)
+            print(guess, 'has earned', str(points), 'points!')
+            score += points
+    print('Total score:', score, 'points')
+
+game()
+
+
+        
+
+
+
+
+
+
 
 
 
